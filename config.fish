c2bed865b9d565fe1fb0c04d9678bd2ca3741db0
Tyler Lawson <tylerjlawson2@gmail.com>
2020-06-26 08:16:19 -0400

    ccat added and some reorginization and slght iterm change to opacity of window

--- config.fish
+++ config.fish
@@ -52,6 +52,7 @@ thefuck --alias | source
 alias python="python3"
 alias hg="history | grep"
 alias dot="cd ~/gitspace/dotfiles"
+alias cat="ccat"
 
 alias gst="git status"
 
--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ cp .vimrc ~
 cp config.fish ~/.config/fish/config.fish
 mkdir -p ~/help
 cp git_shortcuts_cheat_sheet.txt ~/help
-echo "installed dotfiles"
+echo "Installed dotfiles"
 
