59a84d38c5382284665ccb7479c0c772e1fe15ce
Tyler <tyler@secondspectrum.com>
2024-04-17 15:17:49 -0700

    add bun to config.fish for path

--- config.fish
+++ config.fish
@@ -129,3 +129,7 @@ set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
