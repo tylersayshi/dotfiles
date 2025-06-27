153cf78fe3cc946a39dca3384faf9c0923fb901a
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-06-26 18:11:25 -0700

    hide go version

--- config.fish
+++ config.fish
@@ -106,6 +106,7 @@ set -g fish_prompt_pwd_dir_length 0
 set -g theme_display_date no
 set -g theme_date_format "+%a %H:%M"
 set -g theme_nerd_fonts yes
+set -g theme_display_go no
 
 fish_add_path $HOME/bin
 
