44ffa557cbfc04b836e7ab497ce0209625dd1842
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-09-11 22:48:32 -0700

    new scripts setup

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 cp .gitconfig ~
 cp .vimrc ~
 mkdir -p ~/.config/fish/conf.d
+mkdir -p ~/bin
+cp bin/* ~/bin/
 cp config.fish ~/.config/fish/config.fish
 cp fnm.fish ~/.config/fish/conf.d/fnm.fish
 echo "Installed dotfiles"
-
